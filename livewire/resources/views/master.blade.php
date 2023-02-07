9315209182fbb374b3453c38f010f92cdede50c9
yasin-faraji <yaowsin@gmail.com>
2023-02-07 22:50:56 +0330

    2-1

---
+++ livewire/resources/views/master.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Livewire Website</title>
+    <livewire:styles />
+</head>
+<body>
+
+    <h2>Livewire Websit</h2>
+
+    <livewire:scripts />
+</body>
+</html>
