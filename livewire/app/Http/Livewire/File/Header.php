55547110f1a2a96b8ca75721f29d916da4ea06ec
yasin-faraji <yaowsin@gmail.com>
2023-02-09 02:00:46 +0330

    2-3

---
+++ livewire/app/Http/Livewire/File/Header.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\File;
+
+use Livewire\Component;
+
+class Header extends Component
+{
+    public function render()
+    {
+        return view('livewire.file.header');
+    }
+}
--- livewire/app/Http/Livewire/Layouts/Header.php
+++ livewire/app/Http/Livewire/Layouts/Header.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Livewire;
+namespace App\Http\Livewire\Layouts;
 
 use Livewire\Component;
 
@@ -8,6 +8,6 @@ class Header extends Component
 {
     public function render()
     {
-        return view('livewire.header');
+        return view('livewire.layouts.header');
     }
 }
