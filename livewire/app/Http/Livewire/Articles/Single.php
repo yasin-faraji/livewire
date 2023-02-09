3552a6ffcb87a81fd58f22d09c5f4a638458a766
yasin-faraji <yaowsin@gmail.com>
2023-02-09 03:59:09 +0330

    3-1

---
+++ livewire/app/Http/Livewire/Articles/Single.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Articles;
+
+use Livewire\Component;
+
+class Single extends Component
+{
+    public function render()
+    {
+        return view('livewire.articles.single');
+    }
+}
---
+++ livewire/app/Http/Livewire/Post/CreatePosts.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Livewire\Post;
+
+use Livewire\Component;
+
+class CreatePosts extends Component
+{
+    public function render()
+    {
+        return <<<'blade'
+            <div>
+                {{-- The best athlete wants his opponent at his best. --}}
+            </div>
+        blade;
+    }
+}
