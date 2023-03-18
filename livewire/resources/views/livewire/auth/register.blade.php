9f124d9a156ab32e4ce153cc8184a9b717b2b277
yasin-faraji <yaowsin@gmail.com>
2023-03-19 01:07:09 +0330

    4-4

--- livewire/resources/views/livewire/auth/register.blade.php
+++ livewire/resources/views/livewire/auth/register.blade.php
@@ -9,21 +9,21 @@
                     <div class="row mb-3">
                         <label for="inputEmail3" class="col-sm-2 col-form-label">Name</label>
                         <div class="col-sm-10">
-                        <input type="text" class="form-control" id="inputEmail3" wire:model='name'>
+                        <input type="text" class="form-control" id="inputEmail3" wire:model.debounce.1000ms='name'>
                         </div>
                     </div>
 
                     <div class="row mb-3">
                       <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
                       <div class="col-sm-10">
-                        <input type="email" class="form-control" id="inputEmail3" wire:model='email'>
+                        <input type="email" class="form-control" id="inputEmail3" wire:model.debounce.1000ms='email'>
                       </div>
                     </div>
 
                     <div class="row mb-3">
                       <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
                       <div class="col-sm-10">
-                        <input type="password" class="form-control" id="inputPassword3" wire:model='password'>
+                        <input type="password" class="form-control" id="inputPassword3" wire:model.debounce.1000ms='password'>
                       </div>
                     </div>
 
